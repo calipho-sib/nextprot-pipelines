35215e4fd73981885fe4e18d988b7b8b6ac110ab
frednikitin <frederic.nikitin@isb-sib.ch>
2019-07-11 15:43:03 +0200

    Start implementation of loading via POST request to NP2 service

--- src/main/java/org/nextprot/pipeline/statement/nxflat/LoadVariantFrequenciesPipeline.java
+++ src/main/java/org/nextprot/pipeline/statement/nxflat/LoadVariantFrequenciesPipeline.java
@@ -1,5 +1,12 @@
 package org.nextprot.pipeline.statement.nxflat;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.entity.EntityBuilder;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.nextprot.commons.statements.Statement;
 import org.nextprot.pipeline.statement.core.Pipeline;
 import org.nextprot.pipeline.statement.core.PipelineBuilder;
@@ -10,44 +17,50 @@ import org.nextprot.pipeline.statement.core.elements.flowable.FlowEventHandler;
 import org.nextprot.pipeline.statement.core.elements.source.Pump;
 import org.nextprot.pipeline.statement.nxflat.source.pump.WebStatementPump;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static java.lang.Thread.sleep;
 import static org.nextprot.pipeline.statement.core.elements.Source.POISONED_STATEMENT;
 
 
-public class LoadVariantFrequencies {
+public class LoadVariantFrequenciesPipeline {
 
 	public static void main(String[] args) throws IOException {
 
 		Timer timer = new Timer();
 
 		// Note: npteam@kant:/share/sib/calipho/nxflat-proxy/statements-downloader/gnomad/2019-03-14/
-		URL url = new URL("http://kant.sib.swiss:9001/gnomad/2019-03-14/chr22.json");
+		URL K22URL = new URL("http://kant.sib.swiss:9001/gnomad/2019-03-14/chr22.json");
+		URL variantFreqsLoadingServiceURL = new URL("http://alpha-api.nextprot.org/statements/variants/frequencies/");
 
-		Pump<Statement> pump = new WebStatementPump(url);
+		Pump<Statement> pump = new WebStatementPump(K22URL);
 
 		try {
 			Pipeline pipeline = new PipelineBuilder()
 					.start(timer)
 					.source(pump)
-					.split(() -> new VariantFrequencySink(1000), 10)
+					.split(() -> new VariantFrequencySink(variantFreqsLoadingServiceURL, 1000), 10)
 					.build();
 
 			pipeline.openValves();
 
 			// Wait for the pipe to complete
 			try {
-				pipeline.waitForThePipesToComplete();
+				pipeline.waitUntilCompletion();
 			} catch (InterruptedException e) {
 				System.err.println("pipeline error: " + e.getMessage());
 			}
 
-			System.out.println("Done in "+timer.getElapsedTimeInMs() + " ms.");
+			System.out.println("Done in " + timer.getElapsedTimeInMs() + " ms.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -55,11 +68,13 @@ public class LoadVariantFrequencies {
 
 	private static class VariantFrequencySink extends Sink {
 
+		private final URL loadingServiceURL;
 		private final int bufferSize;
 
-		private VariantFrequencySink(int bufferSize) {
+		private VariantFrequencySink(URL loadingServiceURL, int bufferSize) {
 
 			this.bufferSize = bufferSize;
+			this.loadingServiceURL = loadingServiceURL;
 		}
 
 		@Override
@@ -71,7 +86,7 @@ public class LoadVariantFrequencies {
 		@Override
 		public VariantFrequencySink duplicate(int newCapacity) {
 
-			return new VariantFrequencySink(this.bufferSize);
+			return new VariantFrequencySink(this.loadingServiceURL, this.bufferSize);
 		}
 	}
 
@@ -79,12 +94,18 @@ public class LoadVariantFrequencies {
 
 		private final List<Statement> buffer;
 		private final int bufferSize;
+		private final URI loadingServiceURI;
 
 		private Valve(VariantFrequencySink sink) {
 			super(sink);
 
 			buffer = new ArrayList<>(sink.bufferSize);
 			this.bufferSize = sink.bufferSize;
+			try {
+				loadingServiceURI = sink.loadingServiceURL.toURI();
+			} catch (URISyntaxException e) {
+				throw new IllegalStateException(sink.loadingServiceURL+": not a valid url");
+			}
 		}
 
 		@Override
@@ -99,7 +120,7 @@ public class LoadVariantFrequencies {
 
 			if (poisoned || buffer.size() % bufferSize == 0) {
 
-				loadStatements();
+				loadStatements(buffer);
 				buffer.clear();
 
 				sleep(100);
@@ -108,14 +129,55 @@ public class LoadVariantFrequencies {
 			return poisoned;
 		}
 
-		private void loadStatements() {
+		private void loadStatements(List<Statement> statements) throws IOException {
 
 			// ... do the API call to the service doing the load (POST json )
 			// allele_count
 			// allele_number
 			// rs_id
+			Map<String, Object> results = postStatements(statements);
 
-			((FlowLog)getFlowEventHandler()).statementsLoaded(buffer);
+			((FlowLog) getFlowEventHandler()).statementsLoaded(statements);
+		}
+
+		public Map<String, Object> postStatements(List<Statement> statements) throws IOException {
+
+			CloseableHttpClient client = HttpClients.createDefault();
+			HttpPost post = new HttpPost(loadingServiceURI);
+			post.setHeader("Accept", "application/json");
+
+			HttpEntity entity = EntityBuilder.create()
+					.setText(Statement.toJsonString(statements))
+					.build();
+
+			post.setEntity(entity);
+
+			CloseableHttpResponse response = client.execute(post);
+			Map<String, Object> rawContent = readRawJsonOutput(response);
+
+			client.close();
+
+			return rawContent;
+		}
+
+		private Map<String, Object> readRawJsonOutput(CloseableHttpResponse response) throws IOException {
+
+			StringBuilder payload;
+
+			try (BufferedReader in = new BufferedReader(
+					new InputStreamReader(response.getEntity().getContent()))) {
+
+				String line;
+				payload = new StringBuilder();
+
+				while ((line = in.readLine()) != null) {
+					payload.append(line);
+					payload.append(System.lineSeparator());
+				}
+			}
+
+			//noinspection unchecked
+			return new ObjectMapper().readValue(payload.toString(), Map.class);
 		}
 
 		@Override
@@ -151,7 +213,7 @@ public class LoadVariantFrequencies {
 
 		void statementsLoaded(List<Statement> statements) {
 
-			sendMessage("load " + statements.size()+" statements");
+			sendMessage("load " + statements.size() + " statements");
 		}
 
 		@Override
